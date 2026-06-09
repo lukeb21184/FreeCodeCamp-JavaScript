56230a62f26e6da361f2191ca09ec4b557b6bdb6
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-09 14:44:01 -0400

    Update script.js

--- VariablesAndStrings/BuildGreetingBot/script.js
+++ VariablesAndStrings/BuildGreetingBot/script.js
@@ -1,4 +1,9 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
 
+let botLocation = "the universe";
 console.log("Allow me to introduce myself.");
 
 const botIntroduction = "My name is " + bot + ".";
